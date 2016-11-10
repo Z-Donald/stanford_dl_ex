648ca7f6a908c69419bf88d7cd00640d9fa18005
unknown <>
2016-11-10 16:58:54 +0800

    Sparse Autoencoder

---
+++ sparseae_exercise/computeNumericalGradient.m
@@ -0,0 +1,37 @@
+function numgrad = computeNumericalGradient(J, theta)
+% numgrad = computeNumericalGradient(J, theta)
+% theta: a vector of parameters
+% J: a function that outputs a real-number. Calling y = J(theta) will return the
+% function value at theta. 
+  
+% Initialize numgrad with zeros
+numgrad = zeros(size(theta));
+
+%% ---------- YOUR CODE HERE --------------------------------------
+% Instructions: 
+% Implement numerical gradient checking, and return the result in numgrad.  
+% (See Section 2.3 of the lecture notes.)
+% You should write code so that numgrad(i) is (the numerical approximation to) the 
+% partial derivative of J with respect to the i-th input argument, evaluated at theta.  
+% I.e., numgrad(i) should be the (approximately) the partial derivative of J with 
+% respect to theta(i).
+%                
+% Hint: You will probably want to compute the elements of numgrad one at a time. 
+
+epsilon = 10e-5;
+num = size(numgrad, 1);
+for i = 1:num
+    temp = zeros(num,1);
+    temp(i) = epsilon;
+    theta_plus = theta + temp;
+    theta_minus = theta - temp;
+    numgrad(i) = (J(theta_plus) - J(theta_minus)) ./ (2 * epsilon);
+end
+
+
+
+
+
+
+%% ---------------------------------------------------------------
+end
---
+++ sparseae_exercise/sampleIMAGES.m
@@ -0,0 +1,70 @@
+function patches = sampleIMAGES()
+% sampleIMAGES
+% Returns 10000 patches for training
+
+load IMAGES;    % load images from disk 
+
+patchsize = 8;  % we'll use 8x8 patches 
+numpatches = 10000;
+
+% Initialize patches with zeros.  Your code will fill in this matrix--one
+% column per patch, 10000 columns. 
+patches = zeros(patchsize*patchsize, numpatches);
+
+%% ---------- YOUR CODE HERE --------------------------------------
+%  Instructions: Fill in the variable called "patches" using data 
+%  from IMAGES.  
+%  
+%  IMAGES is a 3D array containing 10 images
+%  For instance, IMAGES(:,:,6) is a 512x512 array containing the 6th image,
+%  and you can type "imagesc(IMAGES(:,:,6)), colormap gray;" to visualize
+%  it. (The contrast on these images look a bit off because they have
+%  been preprocessed using using "whitening."  See the lecture notes for
+%  more details.) As a second example, IMAGES(21:30,21:30,1) is an image
+%  patch corresponding to the pixels in the block (21,21) to (30,30) of
+%  Image 1
+
+[r,c,h] = size(IMAGES);
+
+image_sampleind = randi(h, 1, 10000);
+row_sampleind = randi(r - patchsize + 1, 1, 10000);
+column_sampleind = randi(c - patchsize + 1, 1, 10000);
+len = patchsize - 1;
+for i = 1:10000
+    patch = IMAGES(row_sampleind(i):row_sampleind(i) + len, ...
+        column_sampleind(i):column_sampleind(i) + len, image_sampleind(i));
+    patches(:,i) = patch(:);
+end
+
+
+
+
+
+
+%% ---------------------------------------------------------------
+% For the autoencoder to work well we need to normalize the data
+% Specifically, since the output of the network is bounded between [0,1]
+% (due to the sigmoid activation function), we have to make sure 
+% the range of pixel values is also bounded between [0,1]
+patches = normalizeData(patches);
+
+end
+
+
+%% ---------------------------------------------------------------
+function patches = normalizeData(patches)
+
+% Squash data to [0.1, 0.9] since we use sigmoid as the activation
+% function in the output layer
+
+% Remove DC (mean of images). 
+patches = bsxfun(@minus, patches, mean(patches));
+
+% Truncate to +/-3 standard deviations and scale to -1 to 1
+pstd = 3 * std(patches(:));
+patches = max(min(patches, pstd), -pstd) / pstd;
+
+% Rescale from [-1,1] to [0.1,0.9]
+patches = (patches + 1) * 0.4 + 0.1;
+
+end
